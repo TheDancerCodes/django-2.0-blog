5099c63f9b368eea6ad85b72824a30368818e871
Roger Taracha <rojtaracha@gmail.com>
2018-07-22 12:42:44 +0300

    [FEATURE] Canonical URLs for Models: [+] get_absolute_url() method added to the Model returns the canonical URL of the object.
    [+] Use get_absolute_url() method in our templates to link to specific posts.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -33,6 +34,15 @@ class Post(models.Model):
                               default='draft')
     objects = models.Manager()  # The default manager.
     published = PublishedManager()  # Our custom manager.
+
+    # Use get_absolute_url() method in our templates to link to specific posts.
+    def get_absolute_url(self):
+        """Method added to the Model returns the canocial URL of the object."""
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
 
     class Meta:
         ordering = ('-publish',)
