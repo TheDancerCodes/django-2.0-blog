2ff1837afaa4afedbb4d1b6d71014da257c01b2f
Roger Taracha <rojtaracha@gmail.com>
2018-07-22 23:27:00 +0300

    [FEATURE] Adding Pagination: [+] Add the built-in django Paginator class in 'views.py'. [+] Create a pagination template and include it in the list template.

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,26 @@
+from django.core.paginator import Paginator, EmptyPage, \
+    PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
 def post_list(request):
     """A View to display a list of posts."""
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 3 posts in each page.
+    page = request.GET.get('page')  # Indicates current page number.
+    try:
+        posts = paginator.page(page)  # Obtain objects for the desired page.
+    except PageNotAnInteger:
+        #  If page is not an integer deliver the first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        #  If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'page': page,
+                   'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
