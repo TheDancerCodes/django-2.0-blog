7ab5fe72a969bd76119296babe92ea67cc10e035
Roger Taracha <rojtaracha@gmail.com>
2018-07-31 10:53:46 +0300

    [FEATURE] Create a Comment System: [+] Create a model to save comments. [+] Run migrations. [+] Register Comment Model in the Admin site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 """Add Blog Model to the administration site."""
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -14,3 +14,12 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Register Comment Model in Admin Site."""
+
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,8 @@ class PublishedManager(models.Manager):
 
     def get_queryset(self):
         """The method returns the QuerySet that will be executed."""
-        return super(PublishedManager, self).get_queryset().filter(status='published')
+        return super(PublishedManager, self).get_queryset() \
+            .filter(status='published')
 
 
 class Post(models.Model):
@@ -48,3 +49,23 @@ class Post(models.Model):
                              self.publish.month,
                              self.publish.day,
                              self.slug])
+
+
+class Comment(models.Model):
+    """Model to store comments."""
+
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
