14b04ef2c33bd90e93d0d3ffb4809b8c65cca5ca
Roger Taracha <rojtaracha@gmail.com>
2018-07-31 11:08:26 +0300

    [FEATURE] Creating forms from Models: [+] Create a form to submit comments & validate the input data.

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,21 @@
 from django import forms
+from .models import Comment
 
 
 class EmailPostForm(forms.Form):
+    """Form to let users share posts by email."""
+
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
+
+
+class CommentForm(forms.ModelForm):
+    """Build a form dynamically from the Comment Model."""
+
+    class Meta:
+        """Indicate shich model to use to build the form."""
+
+        model = Comment
+        fields = ('name', 'email', 'body')  # List of fields to include in form
