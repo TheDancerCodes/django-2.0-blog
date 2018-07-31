fa142ae4affaa949879ad7d5c7b92cc3e6ccc419
Roger Taracha <rojtaracha@gmail.com>
2018-07-31 11:40:59 +0300

    [FEATURE] Handling ModelForms in views: [+] Use post detail view to instatiate the form and process it. [+] Our view will now display and process new comments.

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.core.paginator import Paginator, EmptyPage, \
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 def post_list(request):
@@ -33,9 +33,35 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+
+            # Assign the current post to the comment
+            new_comment.post = post
+
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form})
 
 
 class PostListView(ListView):
