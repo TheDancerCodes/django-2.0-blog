78bc351c378ffc894e7eb6153250e0b372ec31fc
Roger Taracha <rojtaracha@gmail.com>
2018-08-13 20:15:10 +0300

    [FEATURE] Retrieving posts by similarity: [+] Retrieve all tags for the current post. [+] Get all posts that are tagged with any of those tags. [+] Exclude the current post from the list to avoid recommending the same post.
    [+] Order the results by the number of tags shared with the current post.
    [+] In case of 2 or more posts with the same number of tags, recommend the most recent post.
    [+] Limit the query to the number of posts we want to recommend.

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from django.core.mail import send_mail
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request, tag_slug=None):
@@ -65,12 +66,20 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
