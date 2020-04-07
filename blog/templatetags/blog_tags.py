ee5fd800bc8cd07c6c5e76eab5f2278a487d62d5
Roger Taracha <rojtaracha@gmail.com>
2020-04-07 14:09:38 +0300

    [FEATURE] Creating custom template tags PT2: * Create a inclusion_tag that displays the latest posts in the sidebar of our blog.
    * Create the latest_posts.html template and leverage it in the sidebar in base.html template.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -8,3 +8,11 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+# An inclusion tag to display the latest posts in the sidebar of our blog.
+# We render a template with context variables returned by the template tag.
+# Specify the template that has to be rendered with the returned values
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
