cecd8c7d6ecc47acd7ca4ef5ce50e2a7773b311a
Roger Taracha <rojtaracha@gmail.com>
2020-04-07 14:29:58 +0300

    [FEATURE] Creating custom template tags PT3: * Create a simple template tag that stores the result in a variable that can be reused rather than directly outputting it.
    * Append the get most commented post functionality to the sidebar in base.html

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count
+
 from ..models import Post
 
 register = template.Library()
@@ -16,3 +18,16 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+# A tag to display the most commented posts.
+# A simple template tag that stores the result in a variable that can be reused
+# rather than directly outputting it.
+# Build a QuerySet using the annotate() function to aggregate the total number of comments
+# for each post.
+# Use the Count aggregation function to store the number of comments in the
+# computed field total_comments for each Post object.
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
