68786362b330ed684e4eda5fdb9233af1a9051a7
Roger Taracha <rojtaracha@gmail.com>
2020-04-09 19:35:40 +0300

    Installing PostgreSQL: * Update settings.py file & modify DATABASES.
    * Apply all database migrations.
    
    * Create a superuser.
    
    * Add some sample blogs.

--- django2blog/settings.py
+++ django2blog/settings.py
@@ -84,8 +84,10 @@ WSGI_APPLICATION = 'django2blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django2blog',
+        'USER': 'django2blog',
+        'PASSWORD': 'django2blog',
     }
 }
 
